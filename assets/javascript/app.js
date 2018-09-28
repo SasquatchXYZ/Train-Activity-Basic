228ceda9a5c0773a4c471d81785d8d517bd2b0a9
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-09-28 04:06:44 -0400

    working modals, edit, and remove buttons, also updates from firebase every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,8 @@ function addTrainData() {
         frequency: $("#frequency-input").val().trim(),
     };
 
+    /*    newTrain.postkey = database.ref('Train-Activity').push().key;*/
+
     database.ref('Train-Activity').push(newTrain);
 
     $("#train-name-input").val("");
@@ -25,18 +27,15 @@ function addTrainData() {
 }
 
 function renderTrains(train, key) {
-    let firstTimeConverted = moment(train[key].first, "HH:mm").subtract(1, "years");
+    // console.log(train);
+    let firstTimeConverted = moment(train[key].first, "HHmm").subtract(1, "years");
     // console.log(`First Train Time: ${firstTimeConverted}`);
-
     let timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
     // console.log(`Time Difference: ${timeDifference}`);
-
     let timeRemaining = timeDifference % train[key].frequency;
     // console.log(timeRemaining);
-
     let timeTilNext = train[key].frequency - timeRemaining;
     // console.log(`Minutes Until Train: ${timeTilNext}`);
-
     let nextTrain = moment().add(timeTilNext, "minutes");
     // console.log(`Arrival Time: ${moment(nextTrain).format("hh:mm A")}`);
 
@@ -46,6 +45,10 @@ function renderTrains(train, key) {
                                 <td>${train[key].frequency}</td>
                                 <td>${moment(nextTrain).format("hh:mm A")}</td>
                                 <td>${timeTilNext}</td>
+                                <td><div class="btn-group btn-group-sm" role="group" aria-label="Manage">
+                                    <button type="button" class="btn btn-secondary" id="edit-train" data-edit="${key}">Edit</button>
+                                    <button type="button" class="btn btn-secondary" id="remove-train" data-remove="${key}">Remove</button>
+                                </div></td>
                               </tr>`);
 }
 
@@ -55,42 +58,20 @@ function checkStatus() {
         .then(function (snapshot) {
             var key = snapshot.key;
             if (key === "null") {
-                console.log("Nothing to Show Here");
             } else {
-                console.log("Data to Display");
+                // console.log("Data to Display");
                 trainCheck.once("value")
                     .then(function (childSnapshot) {
                         let train = childSnapshot.val();
-                        console.log(train);
+                        // console.log(train);
                         $(".table tbody").empty();
                         for (var key in train) {
                             if (train.hasOwnProperty(key)) {
-                                console.log(train);
+                                // console.log(train);
+                                // console.log(key);
 
                                 renderTrains(train, key);
 
-/*                                let firstTimeConverted = moment(train[key].first, "HH:mm").subtract(1, "years");
-                                // console.log(`First Train Time: ${firstTimeConverted}`);
-
-                                let timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
-                                // console.log(`Time Difference: ${timeDifference}`);
-
-                                let timeRemaining = timeDifference % train[key].frequency;
-                                // console.log(timeRemaining);
-
-                                let timeTilNext = train[key].frequency - timeRemaining;
-                                // console.log(`Minutes Until Train: ${timeTilNext}`);
-
-                                let nextTrain = moment().add(timeTilNext, "minutes");
-                                // console.log(`Arrival Time: ${moment(nextTrain).format("hh:mm A")}`);
-
-                                $(".table tbody").append(`<tr>
-                                <td>${train[key].name}</td>
-                                <td>${train[key].destination}</td>
-                                <td>${train[key].frequency}</td>
-                                <td>${moment(nextTrain).format("hh:mm A")}</td>
-                                <td>${timeTilNext}</td>
-                              </tr>`);*/
                             }
 
                         }
@@ -104,30 +85,75 @@ function checkStatus() {
 
 $("#add-train").on("click", function (event) {
     event.preventDefault();
-
-    addTrainData();
+    if (($("#train-name-input").val() === "") || ($("#destination-input").val() === "") ||
+        ($("#first-time-input").val()) === "" || ($("#frequency-input").val() === "")) {
+        $('.modal').modal('show');
+    } else {
+        addTrainData()
+    }
 
 });
 
+$(document).on("click", "#remove-train", function () {
+    let postID = $(this).attr("data-remove");
+    console.log(postID);
+
+    let trainRemove = firebase.database().ref(`Train-Activity/${postID}`);
+    trainRemove.remove()
+        .then(function () {
+            console.log('Remove Succeeded.');
+            checkStatus();
+        })
+        .catch(function (error) {
+            console.log('Remove failed: ' + error.message)
+        })
+})
+
+    .on("click", "#edit-train", function () {
+        let postID = $(this).attr("data-edit");
+        console.log(postID);
+        $("#edit-modal").modal("show");
+        $("#update-data").attr("data-update", postID);
+    })
+
+    .on("click", "#update-data", function () {
+        let postID = $(this).attr("data-update");
+        console.log(postID);
+
+        if (($("#train-name-update").val() === "") || ($("#destination-update").val() === "") ||
+            ($("#first-time-update").val()) === "" || ($("#frequency-update").val() === "")) {
+            $('#edit-modal').modal('show');
+        } else {
+            let updatedTrain = {
+                name: $("#train-name-update").val().trim(),
+                destination: $("#destination-update").val().trim(),
+                first: $("#first-time-update").val().trim(),
+                frequency: $("#frequency-update").val().trim()
+            };
+            console.log(updatedTrain);
+
+            $("#train-name-update").val("");
+            $("#destination-update").val("");
+            $("#first-time-update").val("");
+            $("#frequency-update").val("");
+
+            database.ref(`Train-Activity/${postID}`).set(updatedTrain);
+            checkStatus();
+        }
+
+    });
+
+
 database.ref('Train-Activity').on("child_added", function (childSnapshot) {
-    let train = [childSnapshot.val()];
-    console.log(childSnapshot.val());
-    renderTrains(train, 0);
+    let train = childSnapshot.val();
+    let key = childSnapshot.key;
+    // console.log(childSnapshot.key);
 
-/*    let firstTimeConverted = moment(train.first, "HH:mm").subtract(1, "years");
-    // console.log(`First Train Time: ${firstTimeConverted}`);
-
+    let firstTimeConverted = moment(train.first, "HHmm").subtract(1, "years");
     let timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log(`Time Difference: ${timeDifference}`);
-
     let timeRemaining = timeDifference % train.frequency;
-    // console.log(timeRemaining);
-
     let timeTilNext = train.frequency - timeRemaining;
-    // console.log(`Minutes Until Train: ${timeTilNext}`);
-
     let nextTrain = moment().add(timeTilNext, "minutes");
-    // console.log(`Arrival Time: ${moment(nextTrain).format("hh:mm A")}`);
 
     $(".table tbody").append(`<tr>
                                 <td>${train.name}</td>
@@ -135,7 +161,12 @@ database.ref('Train-Activity').on("child_added", function (childSnapshot) {
                                 <td>${train.frequency}</td>
                                 <td>${moment(nextTrain).format("hh:mm A")}</td>
                                 <td>${timeTilNext}</td>
-                              </tr>`);*/
+                                <td><div class="btn-group btn-group-sm" role="group" aria-label="Manage">
+                                        <button type="button" class="btn btn-secondary" id="edit-train" data-edit="${key}">Edit</button>
+                                        <button type="button" class="btn btn-secondary" id="remove-train" data-remove="${key}">Remove</button>
+                                    </div></td>
+                              </tr>`);
+
 }, function (errorObject) {
     console.log(`Errors Handled: ${errorObject.code}`);
 });
